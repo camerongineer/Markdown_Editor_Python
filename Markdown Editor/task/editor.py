202d67237f60e2df54b3eef3174d0a8b0157dea3
camerongineer <93474097+camerongineer@users.noreply.github.com>
2023-04-17 15:12:55 -0700

    Add save method

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,5 +1,5 @@
-import format
 from format import formats
+from format import get_formatted_string
 
 
 def help_command():
@@ -12,19 +12,26 @@ def done_command():
 
 
 def menu():
-    formatted_string = ''
+    formatted_lines = []
     while True:
-        user_input = input('Choose a formatter: ')
+        user_input = input('\nChoose a formatter: ')
         if user_input in formats.keys():
-            formatted_string += format.get_formatted_string(user_input)
-            print(formatted_string)
+            formatted_lines.append(get_formatted_string(user_input))
+            for line in formatted_lines:
+                print(line, end='')
         elif user_input == '!help':
             help_command()
         elif user_input == '!done':
+            save(formatted_lines)
             done_command()
             break
         else:
             print('Unknown formatting type or command')
+
+
+def save(formatted_lines):
+    with open('output.md', 'w') as file:
+        file.writelines(formatted_lines)
 
 
 if __name__ == '__main__':
